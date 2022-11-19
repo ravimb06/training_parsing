df983ec03076183cb84b999cce1976f58e00ef60
Ali Bogatyrev <marksheyder06@gmail.com>
2022-11-19 17:35:14 +0300

    added progress-bar

--- whats_new.py
+++ whats_new.py
@@ -1,6 +1,7 @@
 import requests_cache
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from tqdm import tqdm
 
 WHATS_NEW_URL = 'https://docs.python.org/3/whatsnew/'
 
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     div_with_ul = main_div.find('div', attrs={'class': 'toctree-wrapper'})
     sections_by_python = div_with_ul.find_all(
                          'li', attrs={'class': 'toctree-l1'})
-    for section in sections_by_python:
+    for section in tqdm(sections_by_python):
         version_a_tag = section.find('a')
         href = version_a_tag['href']
         version_link = urljoin(WHATS_NEW_URL, href)
