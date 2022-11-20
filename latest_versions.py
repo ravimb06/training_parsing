a13727d2429cc2fde7c4b09a01543742cb2be182
Ali Bogatyrev <marksheyder06@gmail.com>
2022-11-20 21:02:01 +0300

    parsing latest versions

---
+++ latest_versions.py
@@ -0,0 +1,39 @@
+import requests_cache
+from bs4 import BeautifulSoup
+import re
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+
+if __name__ == '__main__':
+    session = requests_cache.CachedSession()
+    response = session.get(MAIN_DOC_URL)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, features='lxml')
+    sidebar = soup.find('div', attrs={'class': 'sphinxsidebarwrapper'})
+    ul_tags = soup.find_all('ul')
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Ничего не нашлось')
+    result = []
+    pattern_r = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    for a_tag in a_tags:
+        text_match = re.search(pattern_r, a_tag.text)
+        if text_match:
+            link = a_tag['href']
+            version = text_match.group(1)
+            status = text_match.group(2)
+            result.append(
+                (link, version, status)
+            )
+        else:
+            link = a_tag['href']
+            version = a_tag.text
+            status = ''
+            result.append(
+                (link, version, status)
+            )
+    for row in result:
+        print(*row)
--- soup.py
+++ soup.py
@@ -1,8 +1,4 @@
 from bs4 import BeautifulSoup
-
-# В HTML-коде не закрыт парный тег <p>.
-simple_html = '<p>Закройте меня кто-нибудь!'
-# Подставьте в одинарные кавычки разные features — 
-# сначала html.parser, а потом lxml.
+simple_html = '<p>Закройте меня кто-нибудь!'# Подставьте в одинарные кавычки разные features — 
 soup = BeautifulSoup(simple_html, features='lxml')
 print(soup)
