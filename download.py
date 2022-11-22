55f609b86951534e424ad0dc842f461bf9bd5c6b
Ali Bogatyrev <marksheyder06@gmail.com>
2022-11-22 19:22:30 +0300

    link search implemented

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import requests_cache
+import re
 from bs4 import BeautifulSoup
 
 DOWNLOADS_URL = 'https://docs.python.org/3/download.html'
@@ -10,3 +11,4 @@ if __name__ == '__main__':
     soup = BeautifulSoup(response.text, features='lxml')
     main_tag = soup.find('div', {'role': 'main'})
     table_tag = soup.find('table', {'class': 'docutils'})
+    pdf_a4_tag = table_tag.find('a', {'href': re.compile(r'.+pdf-a4\.zip$')})
